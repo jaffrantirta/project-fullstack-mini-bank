a6d2d0f784f299b2eaa318546439fbe41a695f0a
jaffrantirta <jaffrantirta@icloud.com>
2023-04-09 16:30:19 +0800

    fixed problem

--- resources/js/Components/Paginate.jsx
+++ resources/js/Components/Paginate.jsx
@@ -9,13 +9,13 @@ function Paginate({ data, className, ...props }) {
                         Previous
                     </span>
                     :
-                    <a href={data.prev_page_url} className="relative hover:bg-secondary hover:text-white rounded-full inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                    <a href={data.prev_page_url} className="relative hover:bg-secondary rounded-full inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                         Previous
                     </a>
                 }
 
                 {data.next_page_url ?
-                    <a href={data.next_page_url} className="relative hover:bg-secondary hover:text-white rounded-full inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                    <a href={data.next_page_url} className="relative hover:bg-secondary rounded-full inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                         Next
                     </a>
                     :
