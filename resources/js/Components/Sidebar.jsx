5da778cca2078f1669cfcde384cbe1cc78cb5963
jaffrantirta <jaffrantirta@icloud.com>
2023-05-05 11:20:15 +0800

    update sidebar

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -10,68 +10,70 @@ export default function Sidebar(props) {
                 <div className="flex items-center justify-center mt-8">
                     <ApplicationLogo className='block h-9 w-auto fill-current text-gray-800' />
                 </div>
-                <nav className="mt-10 flex flex-col">
-                    <SideLink href={route('dashboard.index')} active={route().current('dashboard.index')}>
-                        <BoltIcon className='h-6 mr-3' />
-                        <span className="text-gray-800 font-medium">Dashboard</span>
-                    </SideLink>
-                    {props.session.roles && props.session.roles.some(role => role.name === 'super-admin') && (
-                        <>
-                            <SideLink href={route('school.index')} active={route().current('school.index')}>
-                                <HomeModernIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Sekolah</span>
-                            </SideLink>
-                            <SideLink href={route('employee.index')} active={route().current('employee.index')}>
-                                <UsersIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Staff Sekolah</span>
-                            </SideLink>
-                            <SideLink href={route('role.index')} active={route().current('role.index')}>
-                                <FingerPrintIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Hak akses</span>
-                            </SideLink>
-                            <SideLink href={route('account.index')} active={route().current('account.index')}>
-                                <RectangleStackIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Akun Transaksi</span>
-                            </SideLink>
-                        </>
-                    )}
+                <div className="h-full overflow-y-auto">
+                    <nav className="mt-10 flex flex-col pb-16">
+                        <SideLink href={route('dashboard.index')} active={route().current('dashboard.index')}>
+                            <BoltIcon className='h-6 mr-3' />
+                            <span className="text-gray-800 font-medium">Dashboard</span>
+                        </SideLink>
+                        {props.session.roles && props.session.roles.some(role => role.name === 'super-admin') && (
+                            <>
+                                <SideLink href={route('school.index')} active={route().current('school.index')}>
+                                    <HomeModernIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Sekolah</span>
+                                </SideLink>
+                                <SideLink href={route('employee.index')} active={route().current('employee.index')}>
+                                    <UsersIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Staff Sekolah</span>
+                                </SideLink>
+                                <SideLink href={route('role.index')} active={route().current('role.index')}>
+                                    <FingerPrintIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Hak akses</span>
+                                </SideLink>
+                                <SideLink href={route('account.index')} active={route().current('account.index')}>
+                                    <RectangleStackIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Akun Transaksi</span>
+                                </SideLink>
+                            </>
+                        )}
 
-                    {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'school-admin') && (
-                        <>
-                            <SideLink href={route('classroom.index')} active={route().current('classroom.index')}>
-                                <BuildingStorefrontIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Kelas</span>
-                            </SideLink>
-                            <SideLink href={route('student.index')} active={route().current('student.index')}>
-                                <UsersIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Siswa</span>
-                            </SideLink>
-                        </>
-                    )}
+                        {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'school-admin') && (
+                            <>
+                                <SideLink href={route('classroom.index')} active={route().current('classroom.index')}>
+                                    <BuildingStorefrontIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Kelas</span>
+                                </SideLink>
+                                <SideLink href={route('student.index')} active={route().current('student.index')}>
+                                    <UsersIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Siswa</span>
+                                </SideLink>
+                            </>
+                        )}
 
 
-                    {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'student-admin' || role.name === 'school-admin') && (
-                        <>
-                            <SideLink href={route('transaction.create')} active={route().current('transaction.create')}>
-                                <DocumentPlusIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Buat Transaksi</span>
-                            </SideLink>
-                            <SideLink href={route('transaction.index')} active={route().current('transaction.index')}>
-                                <DocumentDuplicateIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Transaksi</span>
-                            </SideLink>
-                        </>
-                    )}
+                        {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'student-admin' || role.name === 'school-admin') && (
+                            <>
+                                <SideLink href={route('transaction.create')} active={route().current('transaction.create')}>
+                                    <DocumentPlusIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Buat Transaksi</span>
+                                </SideLink>
+                                <SideLink href={route('transaction.index')} active={route().current('transaction.index')}>
+                                    <DocumentDuplicateIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Transaksi</span>
+                                </SideLink>
+                            </>
+                        )}
 
-                    {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'school-admin') && (
-                        <>
-                            <SideLink href={route('report.index')} active={route().current('report.index')}>
-                                <ChartBarSquareIcon className='h-6 mr-3' />
-                                <span className="text-gray-600">Laporan</span>
-                            </SideLink>
-                        </>
-                    )}
-                </nav>
+                        {props.session.roles && props.session.roles.some(role => role.name === 'super-admin' || role.name === 'school-admin') && (
+                            <>
+                                <SideLink href={route('report.index')} active={route().current('report.index')}>
+                                    <ChartBarSquareIcon className='h-6 mr-3' />
+                                    <span className="text-gray-600">Laporan</span>
+                                </SideLink>
+                            </>
+                        )}
+                    </nav>
+                </div>
             </div>
         </div>
 
